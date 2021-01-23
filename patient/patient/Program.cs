af2d40af1481eedd536029eb0c36bd77f1c4127b
rodman864 <59933094+rodman864@users.noreply.github.com>
2021-01-23 07:46:50 -0500

    Add files via upload

---
+++ patient/patient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace patient
+{
+    public class Hospital
+    {
+        private Queue<int> waitingRoomQueue = new Queue<int>();
+
+        public void addPatientToQueue(int parameter)
+        {
+            waitingRoomQueue.Enqueue(parameter);
+        }
+
+        public void showPaitent()
+        {
+            Console.Write("I have these many" +
+                                 waitingRoomQueue.Count +
+                                     "patients");
+        }
+        public void actualPatients()
+        {
+            foreach(var item in waitingRoomQueue)
+            {
+                Console.WriteLine("Patient: " + item);
+            }
+        }
+        public void patientSeen()
+        {
+            waitingRoomQueue.Dequeue();
+        }
+
+        static void Main(string[] args)
+        {
+            Hospital hospitalPatient = new Hospital();
+            hospitalPatient.addPatientToQueue(1);
+            hospitalPatient.addPatientToQueue(2);
+            hospitalPatient.addPatientToQueue(3);
+            hospitalPatient.addPatientToQueue(4);
+            hospitalPatient.addPatientToQueue(5);
+            hospitalPatient.addPatientToQueue(6);
+
+            hospitalPatient.patientSeen();
+
+            hospitalPatient.actualPatients();
+
+
+
+
+        }
+    }
+}
